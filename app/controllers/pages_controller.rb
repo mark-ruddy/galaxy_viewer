f0c6bf12665afefd65852cb6bdde11b015a22ed1
mark-ruddy <1markruddy@gmail.com>
2021-05-17 14:26:02 +0100

    APOD added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,38 @@
 class PagesController < ApplicationController
+  @planetary = NasaApi::Planetary.new  
+  @mars = NasaApi::Mars.new
+  @error_msg = "The submitted information was invalid or the demo API key has expired"
+
   def index
-    planetary = NasaApi::Planetary.new  
+    if params[:date].present?
+      @apod = @planetary.apod(date: params[:date])
+    else
+      @apod = @planetary.apod
+    end
+  end
+
+  def epic
+    if params[:date].present?
+      @epic = @planetary.epic(date: params[:date])
+    else
+      @epic = @planetary.epic
+    end
+    @latest_epic_image = @epic.image_url[-1] if @epic.image_url
   end
 
   def earth
+    if params[:lat].present? && params[:lon].present?
+      @earth = @planetary.earth_imagery(lat: params[:lat], lon: params[:lon])
+    else
+      @earth = @planetary.earth_imagery(lat: 50, lon: 50)
+    end
   end
 
   def mars
+    if params[:sol].present?
+      @rover = @mars.photos(rover: 'curiosity', sol: params[:sol], camera: 'fhaz')
+    else
+      @rover = @mars.photos(rover: 'curiousity', sol: 1000, camera: 'fhaz')
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+  get '/earth', to:  'pages#earth', as: :earth
+  get '/mars', to: 'pages#mars', as: :mars
 end
