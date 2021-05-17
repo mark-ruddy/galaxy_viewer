5996d3956d9ac1c219fba9c29a56f80859beeb3e
mark-ruddy <1markruddy@gmail.com>
2021-05-17 16:07:07 +0100

    update request specs

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,33 @@
 require 'rails_helper'
 
+# Not testing nasa_api gem in the Rails specs
 RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
+  describe "GET root" do
     it "returns http success" do
-      get "/pages/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe "GET epic" do
+    it "returns http success" do
+      get "/epic"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET earth" do
+    it "returns http success" do
+      get "/earth"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET mars" do
+    it "returns http success" do
+      get "/mars"
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
+
